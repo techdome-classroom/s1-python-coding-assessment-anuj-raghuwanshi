3ea33f0abb1027f386b054ea9291a555e4a61daa
anuj-raghuwanshi <0112cs211025@gmail.com>
2024-09-25 13:38:21 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,49 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False] * cols for _ in range(rows)]
+
+        def dfs(r: int, c: int):
+           
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W' or visited[r][c]:
+                return
+           
+            visited[r][c] = True
+            
+           
+            dfs(r + 1, c)  
+            dfs(r - 1, c)  
+            dfs(r, c + 1)  
+            dfs(r, c - 1)  
+
+        island_count = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 'L' and not visited[i][j]:
+                
+                    dfs(i, j)
+                    island_count += 1 
+
+        return island_count
+
+
+solution = Solution()
+dispatch1 = [
+    ["L", "L", "L", "L", "W"],
+    ["L", "L", "W", "L", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+]
+
+dispatch2 = [
+    ["L", "L", "W", "W", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "L", "W", "W"],
+    ["W", "W", "W", "L", "L"],
+]
+
+
